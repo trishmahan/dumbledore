0d054ef80a2bc556878b15cf729eb063e2a0ffe7
Trish <trish@maxxpotential.com>
2020-12-04 08:43:00 -0500

    add tell me something command

--- dumbledore.py
+++ dumbledore.py
@@ -25,8 +25,7 @@ def speak(text):
     engine.runAndWait()
 
 dumbledoreQuotes = ["It does not do... to dwell on dreams... and forget to live", 
-        "Happiness can be found, even in the darkest of times, if one only remembers to turn on the light.", 
-        "Numbing the pain for a while will make it worse when you finally fee it.", 
+        "Happiness can be found, even in the darkest of times, if one only remembers to turn on the light.",  
         "We must all face the choice between... what is right... and what is easy", 
         "It is our choices that show what we truly are, far more than our abilities", 
         "Words are, in my not-so-humble opinion, our most inexhaustible source of magic, capable of both inflicting injury and remedying it", 
@@ -36,7 +35,6 @@ dumbledoreQuotes = ["It does not do... to dwell on dreams... and forget to live"
         "It is the unknown we fear when we look upon death and darkness. Nothing more",
         "After all, to the well organized mind, death is but the next great adventure",
         "It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends"]
-#speak(dumbledoreQuotes[random.randint(len(dumbledoreQuotes))])
 
 def greetUser(name):
     hour = datetime.datetime.now().hour
@@ -47,31 +45,41 @@ def greetUser(name):
         greeting += " Good Afternoon"
     else:
          greeting += " Good Evening"
+    greeting += ", how can I help you"
     speak(greeting)
 
-def takeCommand():
+def takeCommand(pause):
+    if pause == 0:
+        pause == 5
     r = sr.Recognizer()
-    with sr.Microphone() as source:
+    mic = sr.Microphone()
+    with mic as source:
         print("Listening...")
-        audio=r.listen(source)
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source, timeout=5, phrase_time_limit=6)
         try:
-            statement = r.recognize_google(audio, language = 'en-in')
-            print(f"user said:{statement}\n")
-        except Exception as e:
+            statement = r.recognize_google(audio)
+            print("user said: " + statement)
+        except LookupError:
             speak("Excuse me, will you please repeat that?")
             return "None"
         return statement
 
-speak("Loading your AI personal assistant Dumbledore")
-greetUser("Trish")
-
+speak("Loading Dumbledore")
+#
 if __name__ == '__main__':
+    name = ""
+    while name == "":
+        speak("Hello, what is your name")
+        name = takeCommand(5).lower()
+        greetUser(name)
     while True:
-        speak("How can I help you?")
-        statement = takeCommand().lower()
+        statement = takeCommand(3).lower()
         if statement == 0:
             continue
         if "good bye" in statement or "ok bye" in statement or "stop" in statement:
             speak("Shutting down now, Good bye!")
             break
+        if "tell me something" in statement or "say something" in statement or "i don't know" in statement:
+            speak(dumbledoreQuotes[random.randint(len(dumbledoreQuotes))])
 time.sleep(3)
